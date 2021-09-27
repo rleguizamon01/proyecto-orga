fa72055154c99c7ebf9293a4b2e822c4f3384123
RLeguizamon <leguizamon.rodolfo@gmail.com>
2021-09-27 03:56:48 -0300

    Almacena los valores ingresados por la consola

--- src/convert.c
+++ src/convert.c
@@ -1,7 +1,40 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#define DEBUG
+
+void leer_consola(int* argc, char* argv[]);
+
+int numero = 0;
+int base_origen = 10;
+int base_destino = 10;
 
 int main(int argc, char* argv[])
 {
-    printf("%d\n", argc);
+    leer_consola(&argc, argv);
+    printf("numero es %d\n base_or es %d\n base_dest es %d\n", numero, base_origen, base_destino);
     return 0;
+}
+
+void leer_consola(int* argc, char* argv[])
+{
+    int i;
+    for(i = 0; i < *argc; i++){
+        #ifdef DEBUG
+         printf("REPETICION NUM %d y el elem es %s\n", i, argv[i]);
+        #endif
+
+        if(!strcasecmp(argv[i], "-n")){
+            #ifdef DEBUG
+             printf("detecto -n %s\n", argv[i]);
+            #endif
+            numero = atoi(argv[i + 1]);
+        }
+        else if(!strcasecmp(argv[i], "-s")){
+            base_origen = atoi(argv[i + 1]);
+        }
+        else if(!strcasecmp(argv[i], "-d")){
+            base_destino = atoi(argv[i + 1]);
+        }
+    }
 }
