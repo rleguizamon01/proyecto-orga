20ba81de58a74d3972f22a4a401fd1d0f9cf5a28
Rodolfo Leguizamon <leguizamon.rodolfo@gmail.com>
2021-10-03 22:52:16 -0300

    Agregué -h y -v

--- src/convert.c
+++ src/convert.c
@@ -5,14 +5,16 @@
 
 void leer_consola(int* argc, char* argv[]);
 
-int numero = 0;
+double numero = 0;
 int base_origen = 10;
 int base_destino = 10;
+int mostrar_pasos = 0;
+int mostrar_ayuda = 0;
 
 int main(int argc, char* argv[])
 {
     leer_consola(&argc, argv);
-    printf("numero es %d\n base_or es %d\n base_dest es %d\n", numero, base_origen, base_destino);
+    printf("numero es %f\nbase_or es %d\nbase_dest es %d\nmostrar_pasos es %d\nmostrar_ayuda es %d", numero, base_origen, base_destino, mostrar_pasos, mostrar_ayuda);
     return 0;
 }
 
@@ -21,20 +23,26 @@ void leer_consola(int* argc, char* argv[])
     int i;
     for(i = 0; i < *argc; i++){
         #ifdef DEBUG
-         printf("REPETICION NUM %d y el elem es %s\n", i, argv[i]);
+         //printf("REPETICION NUM %d y el elem es %s\n", i, argv[i]);
         #endif
 
         if(!strcasecmp(argv[i], "-n")){
             #ifdef DEBUG
              printf("detecto -n %s\n", argv[i]);
             #endif
-            numero = atoi(argv[i + 1]);
+            numero = atof(argv[i + 1]);
         }
         else if(!strcasecmp(argv[i], "-s")){
             base_origen = atoi(argv[i + 1]);
         }
         else if(!strcasecmp(argv[i], "-d")){
             base_destino = atoi(argv[i + 1]);
+        }
+        else if(!strcasecmp(argv[i], "-v")){
+            mostrar_pasos = 1;
+        }
+        else if(!strcasecmp(argv[i], "-h")){
+            mostrar_ayuda = 1;
         }
     }
 }
