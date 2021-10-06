6f0e00f76123cfc927d9b4baee4b17e26ebaf6d1
octavioantich <62580250+octavioantich@users.noreply.github.com>
2021-10-06 07:34:27 -0300

    i modified most of the methods, using pointers

--- cosasPara Agregar.c
+++ cosasPara Agregar.c
@@ -1,57 +1,139 @@
 #include <stdio.h>
+#include <stdlib.h>
 #define precisionEntera 10
 #define precisionFraccional 5
+#define ERROR_NO_HAY_MEMORIA 1
 
-void imprimir_arreglo_char(char* num,int tam){
-    printf("------------- \n",tam);
-    for(int i=0;i<tam;i++){
-        printf("%c ",num[i]);
+void imprimir_arreglo_char(char* num,int *tam){
+
+    char * aux;
+    aux=(void*) malloc (sizeof(char));
+    if (aux==NULL)
+        exit(ERROR_NO_HAY_MEMORIA);
+    aux=num;
+
+    int * i;
+    i=(void*) malloc (sizeof(int));
+    if (i==NULL)
+        exit(ERROR_NO_HAY_MEMORIA);
+
+    printf("------------- \n");
+    for(*i=0;*i<*tam;(*i)++){
+        printf("%c ",*aux);
+        aux++;
     }
-    printf("\n------------- \n",tam);
+    printf("\n------------- \n");
+
+    free(aux);
+    free(i);
 }
 
-void imprimir_arreglo_int(int* num,int tam){
-    printf("------------- \n",tam);
-    for(int i=0;i<tam;i++){
-        printf("%d ",num[i]);
+void imprimir_arreglo_int(int* num,int * tam){
+
+    int * aux;
+    aux=(void*) malloc (sizeof(int));
+    if (aux==NULL)
+        exit(ERROR_NO_HAY_MEMORIA);
+    aux=num;
+
+    int * i;
+    i=(void*) malloc (sizeof(int));
+    if (i==NULL)
+        exit(ERROR_NO_HAY_MEMORIA);
+
+    printf("------------- \n");
+    for(*i=0;*i<*tam;(*i)++){
+        printf("%d ",*aux);
+        aux++;
     }
-    printf("\n------------- \n",tam);
+    printf("\n------------- \n");
+
+    free(aux);
+    free(i);
 }
 
-int char_to_int(char c) {
-    int num = c;
+void char_to_int(char * c,int * num) {
 
-    if( (48<=num) && (num <=57)) {
-        num -= 48;
-    } else if( (65<=num) && (num <=70)) {
-        num -= 55;
-    } else if( (97<=num) && (num <=102)) {
-        num -= 87;
+
+    *num=*c;
+
+    if( (48<=*num) && (*num <=57)) {
+        *num -= 48;
+    } else if( (65<=*num) && (*num <=70)) {
+        *num -= 55;
+    } else if( (97<=*num) && (*num <=102)) {
+        *num -= 87;
     }
 
-    return num;
 }
 
-char int_to_char(int num) {
-    char c = num;
-    if(10<=num)
-        c += 55;
+void int_to_char(int * num,char * c) {
+
+    *c=*num;
+
+    if(10<=*num)
+        *c += 55;
     else
-        c += 48;
-    return c;
+        *c += 48;
+
 }
 
+void arreglo_char_to_int(char* arrViejo,int* arrNuevo,int * tam){
+    char * aux;
+    aux=(void*) malloc (sizeof(char));
+    if (aux==NULL)
+        exit(ERROR_NO_HAY_MEMORIA);
+    aux=arrViejo;
 
-void arreglo_char_to_int(char* arrViejo,int* arrNuevo,int tam){
-    for(int i=0;i<tam;i++){
-        arrNuevo[i]=char_to_int(arrViejo[i]);
+    int * aux2;
+    aux2=(void*) malloc (sizeof(int));
+    if (aux2==NULL)
+        exit(ERROR_NO_HAY_MEMORIA);
+    aux2=arrNuevo;
+
+    int * i;
+    i=(void*) malloc (sizeof(int));
+    if (i==NULL)
+        exit(ERROR_NO_HAY_MEMORIA);
+
+    for(*i=0;*i< *tam;(*i)++){
+        char_to_int(aux,aux2);
+        aux2++;
+        aux++;
     }
+
+    free(aux);
+    free(aux2);
+    free(i);
 }
 
-void arreglo_int_to_char(int* arrViejo,char* arrNuevo,int tam){
-    for(int i=0;i<tam;i++){
-        arrNuevo[i]=int_to_char(arrViejo[i]);
+void arreglo_int_to_char(int* arrViejo,char* arrNuevo,int * tam){
+    int * aux;
+    aux=(void*) malloc (sizeof(int));
+    if (aux==NULL)
+        exit(ERROR_NO_HAY_MEMORIA);
+    aux=arrViejo;
+
+    char * aux2;
+    aux2=(void*) malloc (sizeof(char));
+    if (aux2==NULL)
+        exit(ERROR_NO_HAY_MEMORIA);
+    aux2=arrNuevo;
+
+    int * i;
+    i=(void*) malloc (sizeof(int));
+    if (i==NULL)
+        exit(ERROR_NO_HAY_MEMORIA);
+
+    for(*i=0;*i< *tam;(*i)++){
+        int_to_char(aux,aux2);
+        aux2++;
+        aux++;
     }
+
+    free(aux);
+    free(aux2);
+    free(i);
 }
 
 void base_a_decimal_entero(int* numeroViejo,int baseOrigen, int* numeroNuevo){
@@ -84,21 +166,35 @@ void decimal_a_base_entero(int* numeroViejo,int baseDestino, int* numeroNuevo){
     }
 }
 
-void base_a_decimal_fraccional(int* numeroViejo,int baseOrigen, int* numeroNuevo){
+void base_a_decimal_fraccional(int* numeroViejo,int *baseOrigen, int* numeroNuevo){
 
-    double aux;
-    aux=0;
-    double factor=baseOrigen;
 
-    for(int i=0;i<precisionFraccional;i++){
-        aux+=numeroViejo[i] / factor;
-        factor*=baseOrigen;
+    double au, *aux;
+    double fac,* factor;
+    int  ii,* i;
+
+    i=&ii;
+
+    aux=&au;
+    *aux=0;
+
+    factor=&fac;
+    *factor=*baseOrigen;
+
+
+    for(*i=0;*i<precisionFraccional;(*i)++){
+        *aux+= *numeroViejo / *factor;
+        *factor*=*baseOrigen;
+        numeroViejo++;
     }
 
-    for(int i=0;i<precisionFraccional;i++){
-        numeroNuevo[i]=aux;
-        aux-=numeroNuevo[i];
-        aux*=10;
+        printf("%f \n",*aux);
+
+    for(*i=0;*i<precisionFraccional;(*i)++){
+        *aux*=10;
+        *numeroNuevo=*aux;
+        *aux-=*numeroNuevo;
+        numeroNuevo++;
     }
 
 }
@@ -150,7 +246,7 @@ void conversor_base_a_base(char* numeroViejo, int baseOrigen, int baseDestino, c
     }
 
     //parteEnteraNueva
-   // parteFraccionalNueva
+    //parteFraccionalNueva
     //numeroConvertido=
 
 }
@@ -159,22 +255,19 @@ int main(void){
 	char numeroChar[precisionEntera]= {'0','0','0','0','0','A', '2', 'd', '4', 'F'};
 	int numeroInt[precisionEntera]= {0,0,0,0,0,2,1,5,3,6};
     int numeroIntConvertido[precisionEntera];
+    char numeroIntImprimible[precisionEntera];
+
+    int bas, *base;
+    base=&bas;
+    *base=5;
 
 	char fraccionChar[precisionFraccional]= {'A', '2', 'd', '4', 'F'};
-	int fraccionInt[precisionFraccional]= {7,6,3,2,0};
+	int fraccionInt[precisionFraccional]= {7,1,4,11,15};
     int fraccionIntConvertido[precisionFraccional];
+    char fraccionIntImprimible[precisionFraccional];
 
-
-    //imprimir_arreglo_char(numeroChar,precisionEntera);
-    //arreglo_char_to_int(numeroChar,numeroInt,precisionEntera);
-    //base_a_decimal_entero(numeroInt,16,numeroIntConvertido);
-
-    //decimal_a_base_entero(numeroInt,8,numeroIntConvertido);
-    //imprimir_arreglo_int(numeroIntConvertido,precisionEntera);
-
-    base_a_decimal_fraccional(fraccionInt,8,fraccionIntConvertido);
-    imprimir_arreglo_int(fraccionIntConvertido,precisionFraccional);
-
+    arreglo_int_to_char(fraccionInt,fraccionIntImprimible,base);
+    imprimir_arreglo_char(fraccionIntImprimible,base);
 
 	system("pause");
 	return (0);
